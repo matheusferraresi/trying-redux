2679758931126f02f55578192a05f6a4522d5dca
Matheus Ferraresi <matheusferraresi@live.com>
2018-09-06 23:06:39 +0100

    Updating state immutably

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,29 +1,47 @@
 const initialState = {
-    counter: 0
+    counter: 0,
+    results: []
 }
 
 const reducer = (state = initialState, action) => {
     switch(action.type) {
         case 'INCREMENT':
             return {
+                ...state,
                 counter: state.counter + 1
             }
 
         case 'DECREMENT':
             return {
+                ...state,
                 counter: state.counter - 1
             }
 
         case 'ADD':
             return {
+                ...state,
                 counter: state.counter + action.value
             }
 
         case 'SUBTRACT':    
             return {
+                ...state,
                 counter: state.counter - action.value
             }
-            
+
+        case 'STORE_RESULT':
+            return {
+                ...state,
+                results: state.results.concat({id: new Date(), value: state.counter})
+            }
+
+        case 'DELETE_RESULT':
+            const updArray =  state.results.filter(result => result.id !== action.id);
+            return {
+                ...state,
+                results: updArray
+            }
+
         default:
             return state; 
     }
